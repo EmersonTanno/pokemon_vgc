f3c8f06d5bcb97e669c5c7088886acdf0cb3cc94
Emerson Tanno <tannobrizotto@gmail.com>
2024-09-13 11:30:48 -0300

    update: home_page

--- lib/app/views/home_page_view.dart
+++ lib/app/views/home_page_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pokemon_vgc/app/controllers/app_controller.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -11,7 +10,6 @@ class HomePage extends StatefulWidget{
 
 
 class HomePageState extends State<HomePage>{
-  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +27,7 @@ class HomePageState extends State<HomePage>{
             title: Text('Main'),
             subtitle: Text('Tela Inicial'),
             onTap: (){
-              Navigator.of(context).pushReplacementNamed('/');
+              Navigator.of(context).pushReplacementNamed('/pokemonTeam');
             },
           ),
           ListTile(
@@ -45,63 +43,18 @@ class HomePageState extends State<HomePage>{
       appBar: AppBar(
         title: Text('PokéCenter'),
         backgroundColor: Colors.red,
-        actions: [CustomSwitcher()],
       ),
       
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Contador $count'),
-          Container(
-            height: 10,
-          ),
-          CustomSwitcher(),
-          Container(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.black,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.red,
-              )
-            ],
-          )
-      ],),
-      ),
+
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add,
         color: Colors.red,),
         onPressed: () {
           setState(() {
-              count++;
             });
         },),
     );
   }
 
-}
-
-class CustomSwitcher extends StatelessWidget {
-  const CustomSwitcher({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Switch(
-          value: AppController.instance.isDartTheme, 
-          onChanged: (value){
-            AppController.instance.changeTheme();
-        },);
-  }
 }
--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:pokemon_vgc/app/views/register_user_view.dart';
-import 'package:pokemon_vgc/app/controllers/app_controller.dart';
 import 'package:pokemon_vgc/app/views/home_page_view.dart';
 import 'package:pokemon_vgc/app/views/team_builder_home_view.dart';
 import 'package:pokemon_vgc/app/views/login_page_view.dart';
 
-class AppWidget extends StatelessWidget{
-
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: AppController.instance,
-      builder: (context, child){
+      animation: Listenable.merge([]),
+      builder: (context, child) {
         return MaterialApp(
           theme: ThemeData(
             primaryColor: Colors.red,
-            brightness: AppController.instance.isDartTheme 
-            ? Brightness.dark 
-            : Brightness.light
           ),
           initialRoute: '/',
           routes: {
@@ -27,7 +22,8 @@ class AppWidget extends StatelessWidget{
             '/pokemonTeam': (context) => TeamBuilderHome(),
           },
         );
-    });
+      },
+      child: null,
+    );
   }
-  
 }
